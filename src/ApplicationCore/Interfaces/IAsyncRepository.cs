889621d268e40cc70bc8032b513c3098ecefbc72
atakan dönmez <atakandmz@gmail.com>
2021-09-23 12:09:43 +0300

    AsyncRepo eklendi

---
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IAsyncRepository<T> where T:BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAllSync();
+        Task<List<T>> ListASync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task CountAsync(ISpecification<T> spec);
+        Task<T> FirstAsync(ISpecification<T> spec);
+        Task <T> FirstOrDefaultAsync(ISpecification<T> spec);
+
+
+
+    }
+}
